b9ceb7f0874b6d9ceda880a2fba821922c2eb074
Abdelhakime6 <abdelhakimeaouzou6@gmail.com>
2023-12-14 22:28:00 +0100

    mno

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var addButton = document.getElementById('addImgBtn');
+    var gallery = document.getElementById('gallery');
+
+    addButton.addEventListener('click', function () {
+        var imgLink = prompt('Enter the link to the image:');
+
+        if (imgLink) {
+            addImageToGallery(imgLink);
+        }
+    });
+function addImageToGallery(imgLink) {
+    console.log("Image link:", imgLink);
+
+    var newAnchor = document.createElement('a');
+    newAnchor.className = 'col-md-12 col-lg-4 img-fluid py-2';
+    newAnchor.setAttribute('data-toggle', 'lightbox');
+    newAnchor.setAttribute('data-gallery', 'photo_gallery');
+    newAnchor.setAttribute('href',imgLink);
+
+    var newImage = document.createElement('img');
+    newImage.src = imgLink;  
+    newImage.alt = 'random img';
+    newImage.className = 'img-fluid';
+
+    newAnchor.appendChild(newImage);
+    gallery.appendChild(newAnchor);
+
+}
+
+});

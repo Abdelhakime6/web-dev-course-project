f64993ee29ac9e8718332351b6a3edf3b47006ae
Abdelhakime6 <abdelhakimeaouzou6@gmail.com>
2023-12-14 20:08:28 +0100

    klm

--- scripty.js
+++ scripty.js
@@ -25,6 +25,5 @@ document.addEventListener('DOMContentLoaded', function () {
         newAnchor.appendChild(newImage);
 
         gallery.appendChild(newAnchor);
-        $(gallery).lightbox();
     }
 });
